d7ecab4237e1542fffa21933e4020cc15619d6ab
esameisa <esameisa12345@gmail.com>
2019-01-27 20:16:04 +0200

    init commit

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
-//set port
-var port = process.env.PORT || 8080
-
-app.use(express.static(__dirname + "/public"));
-
-//route
-app.get("/", function (req, res) {
-    res.render("index");
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(port, function () {
-    console.log("app running");
-})
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
